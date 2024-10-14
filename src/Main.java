53bae5e4a0395856ab0779684c37d628b775763e
BrycenP <perezb3@mail.uc.edu>
2024-10-13 21:21:52 -0400

    Changed win messages to match requirements

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,18 @@ public class Main {
         do {
             System.out.print("Player A, what is your move [please use R,P,or S]");
             playerA = in.next();
+
             System.out.print("Player B, what is your move [please use R,P,or S]");
             playerB = in.next();
 
             if (playerA.equalsIgnoreCase("r") && playerB.equalsIgnoreCase("r")) {
                 System.out.println("Rock vs Rock is a tie");
             } else if (playerA.equalsIgnoreCase("p") && playerB.equalsIgnoreCase("r")) {
-                System.out.println("Paper beats Rock, Player A wins");
+                System.out.println("Paper covers Rock, Player A wins");
             } else if (playerA.equalsIgnoreCase("s") && playerB.equalsIgnoreCase("r")) {
                 System.out.println("Rock breaks Scissors, Player B wins");
             } else if (playerA.equalsIgnoreCase("r") && playerB.equalsIgnoreCase("p")) {
-                System.out.println("Paper beats Rock, Player B wins");
+                System.out.println("Paper covers Rock, Player B wins");
             } else if (playerA.equalsIgnoreCase("p") && playerB.equalsIgnoreCase("p")) {
                 System.out.println("Paper vs Paper is a tie");
             } else if (playerA.equalsIgnoreCase("s") && playerB.equalsIgnoreCase("p")) {
